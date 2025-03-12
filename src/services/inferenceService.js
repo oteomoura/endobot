ffe1e64c45dcd0338a3731019c9ce39794fa33fa
Teógenes Moura <mourateogenes@gmail.com>
2025-03-12 12:32:33 -0300

    Generate augmented chat response based on user input

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -1,13 +1,38 @@
-import { togetherAiClient, INFERENCE_MODEL_URL } from '../config/togetherAi.js';
+import { togetherAiClient, COMPLETIONS_API_URL } from '../config/togetherAi.js';
 
-export async function generateAnswer(prompt) {
+const buildSystemPrompt = () => ({
+  role: 'system',
+  content: `Você é uma assistente de saúde expert em endometriose, 
+  saúde da mulher e dor cronica. Você vai interagir com mulheres entre 18 e 55 anos, 
+  que sofrem ou desconfiam sofrer de Endometriose, dor crônica ou outras doenças 
+  relacionadas. O seu tom deve ser amigável e de fácil compreensão. 
+  Não responda a perguntas que não sejam relacionadas a endometriose, 
+  dor crônica ou saúde da mulher.`  
+})
+
+const buildAssistantPrompt = (context) => ({
+  role: 'assistant',
+  content: `Aqui está contexto relevante para a sua resposta:  ${context}`
+})
+
+const buildUserPrompt = (userMessage) => ({
+  role: 'user',
+  content: userMessage
+})
+
+export async function generateAnswer(context, userMessage) {
+  const systemPrompt = buildSystemPrompt();
+  const assistantPrompt = buildAssistantPrompt(context);
+  const userPrompt = buildUserPrompt(userMessage);
+  
   try {
-    const response = await togetherAiClient.post(INFERENCE_MODEL_URL, {
-      prompt,
-      max_tokens: 150,
+    const response = await togetherAiClient.post(COMPLETIONS_API_URL, {
+      model: 'meta-llama/Llama-3.3-70B-Instruct-Turbo-Free',
+      context_length_exceeded_behavior: 'error',
+      messages: [systemPrompt, assistantPrompt, userPrompt]
     });
 
-    return response.data.choices[0].text.trim();
+    return response?.data?.choices?.[0]?.message?.content || 'No content available';  
   } catch (error) {
     console.error('Error generating answer:', error);
     throw error;
